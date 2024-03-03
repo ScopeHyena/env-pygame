3723d07f08fa19783142dfc1a93c5106d32f5077
Scope.h <hongsch07@naver.com>
2024-03-03 22:25:34 +0900

    Update Main.py
    code correction

--- scripts/Main.py
+++ scripts/Main.py
@@ -11,7 +11,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit()
+            sys.exit()
 
     screen.fill('black')
     level.run()
